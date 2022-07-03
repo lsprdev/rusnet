359959c930849c5326697a8dedccaf477ec769d8
OGabrielPereira <ogabrielpereira@pm.me>
2022-07-03 10:54:37 -0300

    GNU License

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 mod ip_fn;
 use std::env;
 
+/*  rusnet -- IPv4 Subnet Calculator
+    Copyright (C) 2022  Gabriel L. Pereira.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
+ */
+
+/*
+ *   author Gabriel L. Pereira <ogabrielpereira@pm.me>
+ *   version 0.1.0
+ *   since Jul 1, 2022
+ */
+
 fn main() {
     let args: Vec<String> = env::args()
                                     .collect();
