630e5b3703eb8fc8291fa41f0a2aa3804a009e0a
OGabrielPereira <ogabrielpereira@pm.me>
2022-07-03 09:58:17 -0300

    comments added

--- src/ip_fn/broadcast.rs
+++ src/ip_fn/broadcast.rs
@@ -1,5 +1,5 @@
 pub fn bc(last_octet_subnet: u16) -> u16 {
-    let qt_hosts = 256;
-    let broadcast_address = (qt_hosts - last_octet_subnet)-1;
+    let qt_hosts = 256; // 0 - 255
+    let broadcast_address = (qt_hosts - last_octet_subnet)-1; 
     broadcast_address
 }
--- src/ip_fn/range.rs
+++ src/ip_fn/range.rs
@@ -2,10 +2,12 @@ pub fn rg(last_octet: u16, last_octet_subnet: u16) -> Vec<u16>{
 
     let qt_hosts = 256;
     let mut range: Vec<u16> = vec![];
-
+    
+    // Start of the range
     let first_range = last_octet + 1;
     range.push(first_range);
-    
+
+    // End of the range
     let last_range = (qt_hosts - last_octet_subnet)-2;
     range.push(last_range);
 
